e224bef8005413167b55cd113534a394e775b814
DenKo45 <denis.korgew234@gmail.com>
2020-06-01 12:37:17 +0300

    add project

---
+++ UT_MyMath/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UT_MyMath")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UT_MyMath")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("1349550d-047e-4d15-a301-22e76b33f000")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Ul/main.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MyMatch;
+
+namespace Ul
+{
+    public partial class main : Form
+    {
+        public main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string error = "";
+            double[] result = KvUr.Matching(int.Parse(textBox1.Text), int.Parse(textBox2.Text), int.Parse(textBox3.Text), out error);
+            string resultStr = string.Join("\n", result);
+
+            MessageBox.Show(resultStr, error);
+        }
+    }
+}
